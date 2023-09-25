2e45ee8f5cf90b5630742041e793d669c0bae8ce
Gelix1470 <gleb.nazarov.1982@mail,ru>
2023-09-25 22:46:44 +0300

    dz2

---
+++ dz2.py
@@ -0,0 +1,11 @@
+def multiplication_table(n):
+    for i in range(1, n + 1):
+        for j in range(1, 10):
+            print(f"{i} * {j} = {i * j}")
+        print('')
+
+
+n = int(input("Введите число n: "))
+multiplication_table(n)
+
+# я выбрал процедурную парадигму, так как она является достаточно простой и понятной для решения этой задачи.
